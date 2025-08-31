afd76488ff51102501ca08057a492769da2e038d
anaskhan206 <anaskhan171200a@email.com>
2025-08-31 20:18:33 +0500

    Removebg and FirstProgram

---
+++ FirstProgram/index.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+
+print("My name is Anas Khan")
+print("My age is 24")
+
+print("My name is Anas Khan", "My age is 24")
+
+print(20)
+print(20+35)
---
+++ Rmebg/index.py
@@ -0,0 +1,124 @@
+from rembg import remove
+from PIL import Image
+
+input_path = 'cars.jpg'
+output_path = 'carss.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'fast.avif'
+output_path = 'fasts.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'gtr.jpg'
+output_path = 'gtr.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'gtrsky.jpg'
+output_path = 'gtrskys.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'mk4.jpg'
+output_path = 'mk4s.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'nissan.jpg'
+output_path = 'nissans.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'nissansky.webp'
+output_path = 'nissanskys.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'r35.webp'
+output_path = 'r35s.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'skyline.webp'
+output_path = 'skylines.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+input_path = 'supra.jpg'
+output_path = 'supras.png'
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+
+output_image.save(output_path)
+
+print("Background successfully removed!")
+
+
+
+
